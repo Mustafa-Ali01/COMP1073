f66efa31057041264e9b12c7110d2e62bd4ebd6f
Ali <200643639@student.georgianc.on.ca>
2026-04-14 21:25:20 -0400

    Still learning how to use this api. To use this api you need to get the latatuide and longatuide of the city then you can effectively use it to get weather, and other stuff from the api once you have those

--- Assignments/Assignment-4/script.js
+++ Assignments/Assignment-4/script.js
@@ -5,6 +5,19 @@ fetchBtn.addEventListener('click', async function() {
     const city = document.getElementById('cityInput').value;
     const display = document.getElementById('displayArea');
 
-    // Logging to ensure the button click is working
-    alert("Searching for city: " + city);
+    // Get Coordinates
+    const geoResponse = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
+    const geoData = await geoResponse.json();
+
+    // Stop the function if the city is not found
+    if (geoData.length === 0) {
+        alert("Location not found");
+        return; 
+    }
+
+    const lat = geoData[0].lat;
+    const lon = geoData[0].lon;
+    const name = geoData[0].name;
+
+    alert("Coordinates found:", lat, lon);
 });
